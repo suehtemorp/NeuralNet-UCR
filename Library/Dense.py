4253523ab88aa4a863d5ce9bf210f2eeb968573b
Javier Solano <javier.solanosaltachin@ucr.ac.cr>
2023-11-10 16:48:23 -0600

    Refactored Dense Layer

---
+++ Library/Dense.py
@@ -0,0 +1,45 @@
+# Numpy numericals
+import numpy as np
+
+# Type hints
+import typing
+
+# Neural Network interfaces
+from .NeuralNetwork import Neural_Layer
+
+class Dense_Layer(Neural_Layer):
+	"""A densely connected, Feed-Forward Neural Network layer"""
+
+	def __init__(
+		self
+        , output_dim : np.uint # Length of output vector
+        , input_dim: np.uint # Length of input vector (merely a hint)
+        , activation_function :  typing.Callable[[float], float] # Activation function
+	):
+		super().__init__(output_dim=output_dim, input_dim=input_dim, activation_function=activation_function)
+		# Weights for this layer's activation
+		self.weights : np.ndarray = np.random.uniform(low=-1, high=1, size=(output_dim, input_dim))
+		# Biases for this layer's activation
+		self.biases : np.ndarray = np.zeros(shape=(output_dim, 1), dtype=np.float64)
+
+	def Predict(
+		self
+		, layer_input : np.ndarray # Vector values aligned with this layer's input dimension
+    ) -> np.ndarray: # Predicted output vector
+		# Compute the weighted sum of the previous layer, and after applying the
+        # activation function, set it as this layer's activation 
+		print(f"{layer_input.shape}, {layer_input}, {self.weights.shape}")
+		weighted_sum = np.dot(layer_input, self.weights) + self.biases
+		self.output = self.activation_function(weighted_sum)
+		
+		print(f"Predict: Layer output shape {self.output.shape}, {self.output}")
+		return self.output
+	
+	def Connect_Layer(
+		self
+		, next_layer: "Neural_Layer" # Layer to connect to.
+	) -> None:
+		# Check if dimension hint was for previous layer is correct
+        # If not, abort connection
+		if self.output_dim != next_layer.input_dim:
+			raise Exception("Previous layer output dimension doesn't match this layer's input dimension")
