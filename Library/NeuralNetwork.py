af093a56dffc460c02ca5896bbb4c8c30002d995
Javier Solano <javier.solanosaltachin@ucr.ac.cr>
2023-11-15 21:19:39 -0600

    Polished interface

--- Library/NeuralNetwork.py
+++ Library/NeuralNetwork.py
@@ -14,7 +14,7 @@ class Neural_Layer (metaclass=AbstractClass):
         self
         , output_dim : np.uint # Length of output vector
         , input_dim: np.uint # Length of input vector (merely a hint)
-        , activation_function :  typing.Callable[[float], float] # Activation function
+        , activation_function :  typing.Callable[[float, bool], float] # Activation function, with optional derivative usage
 	):
 		"""Construct a neural network layer"""
 		# Node count of nodes inside layer
@@ -24,14 +24,15 @@ class Neural_Layer (metaclass=AbstractClass):
 		# Activation function
 		self.activation_function : np.uint = activation_function
 		# Output values stored inside layer
-		self.last_output = np.zeros(shape=(output_dim, 1), dtype=np.float64)
-		self.last_input : np.ndarray
+		self.last_output : np.ndarray = np.zeros(shape=(output_dim, 1), dtype=np.float64)
+		self.last_preactivations : np.ndarray = np.zeros(shape=(output_dim, 1), dtype=np.float64)
+		self.last_input : np.ndarray # Shape decided by implementation
 
 	@AbstractMethod
 	def Predict(
 		self
-		, layer_input : np.ndarray # Matrix, with each row representing an input vector
-	) -> np.ndarray: # Matrix, with each row representing an output vector
+		, layer_input : np.ndarray # Matrix, with each column representing an input vector
+	) -> np.ndarray: # Matrix, with each column representing an output vector
 		"""Compute an individual output based on an individual input"""
 		pass
 
@@ -41,6 +42,13 @@ class Neural_Layer (metaclass=AbstractClass):
 		, next_layer: "Neural_Layer" # Layer to connect to.
 	) -> None:
 		"""Connects Next layer with current layer"""
+		pass
+
+	@AbstractMethod
+	def Gradients_From_Activation(
+		self
+	) -> np.ndarray: # Matrix, with each column representing the gradient vector for each output of this layer
+		"""Compute a matrix of Gradients, one on each column, from the previous input of this layer"""
 		pass
 
 # Base definition for a neural network
@@ -62,8 +70,8 @@ class Neural_Network (metaclass=AbstractClass):
 
 	@AbstractMethod
 	def Cost_Overall(
-		computed_outputs : np.ndarray # Matrix, with each row representing an output vector
-		, expected_outputs : np.ndarray # Matrix, with each row representing an output vector
+		computed_outputs : np.ndarray # Matrix, with each column representing an output vector
+		, expected_outputs : np.ndarray # Matrix, with each column representing an output vector
 	) -> np.float64: # Computed cost over all input-output pairs
 		"""Compute the cost function based on the set of outputs and the expected outputs"""
 		pass
@@ -71,8 +79,8 @@ class Neural_Network (metaclass=AbstractClass):
 	@AbstractMethod
 	def Train(
 		self
-		, inputs : np.ndarray # Matrix, with each row representing an input vector
-		, outputs : np.ndarray # Matrix, with each row representing an output vector
+		, inputs : np.ndarray # Matrix, with each column representing an input vector
+		, outputs : np.ndarray # Matrix, with each column representing an output vector
 		, learning_rate : np.float64 # Learning rate. Bigger values mean higher rate
 		, epochs : np.uint64 # Iteration count for training backpropagation cycles
 		, epoch_report_rate : np.uint = 1 # How many epochs until next error report
@@ -83,7 +91,7 @@ class Neural_Network (metaclass=AbstractClass):
 	@AbstractMethod
 	def Predict(
 		self
-		, inputs : np.ndarray # Matrix, with each row representing an input vector
-	) -> np.ndarray: # Matrix, with each row representing an output vector
+		, inputs : np.ndarray # Matrix, with each column representing an input vector
+	) -> np.ndarray: # Matrix, with each column representing an output vector
 		"""Compute a set of outputs based on a set of inputs"""
 		pass
