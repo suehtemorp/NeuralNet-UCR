cfc9024183730f09f38e104b8584b4d70abe0aca
Javier Solano <javier.solanosaltachin@ucr.ac.cr>
2023-11-15 21:20:39 -0600

    Added misc includes

--- Library/__init__.py
+++ Library/__init__.py
@@ -1,4 +1,4 @@
 from .NeuralNetwork import Neural_Layer, Neural_Network
 from .Sequential import Sequential
 from .Dense import Dense_Layer
-from .ActivationFunctions import sigmoid
+from .ActivationFunctions import sigmoid, relu
